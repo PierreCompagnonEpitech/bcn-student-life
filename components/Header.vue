3c4db8341dba83840458221e8dae717be455edca
kevinnassara <nassarakevin@gmail.com>
2024-04-13 22:19:42 +0200

    first block

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex h-20 shadow-2xl w-full justify-center">
+        <div class="flex flex-row items-center h-full bg-white justify-between w-4/5">
+         
+            <div @click="$router.push('/')" id="Logo" class="cursor-pointer lg:text-2xl barlow-black">
+                BCN Student Life
+            </div>
+
+            <div class="flex flex-row w-1/2 justify-around">
+                <nav class="hover:text-pink-600 lg:text-base text-xs cursor-pointer">Live in Barcelona</nav>
+                <nav class="hover:text-pink-600 lg:text-base text-xs cursor-pointer">Explore Barcelona</nav>
+                <nav class="hover:text-pink-600 lg:text-base text-xs cursor-pointer">Tips and Tricks</nav>
+            </div>
+
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,28 @@
 <template>
     <div>
-        ok
+        <Header />
+    
+    <div id="FirstBlock" class="flex flex-col md:flex-row mt-12 justify-center justify-betwee">
+
+        <div id="Text1" class="flex flex-col lg:justify-start m-auto lg:mt-12 barlow-bold justify-center items-center w-full lg:w-1/2">
+            <div class="text-3xl lg:text-6xl w-5/6">
+                <b class="text-pink-600">Barcelona:</b> Get ready for your dream <b class="text-yellow-500">student adventure!</b>
+            </div>
+            <div class="text-gray-500 lg:text-xl w-5/6 font-normal mt-2">
+                <em>Studying in Barcelona ?</em> Your ultimate guide to setting in, integrating and making the most of this incredible city.
+            </div>
+        </div>
+
+        <div id="Images1" data-aos="flip-u" class="flex m-auto">
+            <NuxtImg
+                src="/Images.png"
+                sizes=""
+            />
+        </div>
+    </div>
+
     </div>
 </template>
+
+<style scoped>
+</style>
